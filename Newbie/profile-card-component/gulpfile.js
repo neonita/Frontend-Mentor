afae1fcb9783bcf2b0d863a5c9c8ca121bd92ce5
neonita <neonita888@gmail.com>
2022-03-21 19:05:08 -0400

    transfered rest of files from previous challenge folder to current one

---
+++ Newbie/profile-card-component/gulpfile.js
@@ -0,0 +1,12 @@
+const { src, dest, watch, series } = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+
+function buildStyle() {
+  return src("index.scss").pipe(sass()).pipe(dest("css"));
+}
+
+function watchTask() {
+  watch(["index.scss"], buildStyle);
+}
+
+exports.default = series(buildStyle, watchTask);
