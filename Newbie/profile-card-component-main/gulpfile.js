b552b1fa16ff131dca4b3870df988992e790afc3
neonita <neonita888@gmail.com>
2022-03-21 16:08:42 -0400

    update watched file's src in gulpfile.js

--- Newbie/profile-card-component-main/gulpfile.js
+++ Newbie/profile-card-component-main/gulpfile.js
@@ -3,12 +3,12 @@ const sass = require("gulp-sass")(require("sass"));
 
 // compiles sass to css
 function buildStyle() {
-  return src("css/index.scss").pipe(sass()).pipe(dest("css"));
+  return src("css/**/*.scss").pipe(sass()).pipe(dest("css"));
 }
 
 // watch sass file for changes and recompiles it at each update
 function watchTask() {
-  watch(["css/index.scss"], buildStyle);
+  watch(["css/**/*.scss"], buildStyle);
 }
 
 // run the files
