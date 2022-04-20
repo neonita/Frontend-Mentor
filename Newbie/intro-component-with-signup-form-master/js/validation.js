bd4be03a246dbf4ea78697d0fce610d1079b4dc1
neonita <neonita888@gmail.com>
2022-04-20 00:49:43 -0400

    Create variables js variables for elements the form and its input elements, in validation.js

---
+++ Newbie/intro-component-with-signup-form-master/js/validation.js
@@ -0,0 +1,8 @@
+const form = document.querySelector(".form");
+const firstName = document.querySelector(".first-name");
+const lastName = document.querySelector(".last-name");
+const email = document.querySelector(".email");
+const password = document.querySelector(".password");
+const btn = document.querySelector(".form__btn");
+
+console.log();
