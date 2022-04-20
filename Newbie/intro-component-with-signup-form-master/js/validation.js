98fc8e60960dfca77f4afd781f6d6e0e59f6f930
neonita <neonita888@gmail.com>
2022-04-20 00:54:46 -0400

    Correct input types of button and password inputs

--- Newbie/intro-component-with-signup-form-master/js/validation.js
+++ Newbie/intro-component-with-signup-form-master/js/validation.js
@@ -5,4 +5,9 @@ const email = document.querySelector(".email");
 const password = document.querySelector(".password");
 const btn = document.querySelector(".form__btn");
 
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  alert("submit button pressed!");
+});
+
 console.log();
