ed7ecb8d1d884cb397de06774cacf1f2f499cab6
Neonita <neonita888@gmail.com>
2022-04-29 00:03:52 -0400

    Add function to button so it generates a new advice and replace quotation marks

--- Junior/advice-generator-app-main/js/script.js
+++ Junior/advice-generator-app-main/js/script.js
@@ -2,14 +2,33 @@ let adviceNum = document.querySelector(".card__number");
 let adviceText = document.querySelector(".card__advice-generated");
 let btn = document.querySelector(".card__btn");
 
+// Generates a random advice so that the
+// advice element is not empty on landing page.
+const randomAdvice = (response) => {
+  const slip = response.data.slip;
+  const adviceId = slip.id;
+  const adviceTxt = slip.advice;
+
+  adviceNum.innerHTML = adviceId;
+  adviceText.innerHTML = adviceTxt;
+};
+
 const showAdvice = (response) => {
   const slip = response.data.slip;
   const adviceId = slip.id;
   const adviceTxt = slip.advice;
+
+  adviceNum.innerHTML = adviceId;
+  adviceText.innerHTML = adviceTxt;
 };
 
+const apiUrl = `https://api.adviceslip.com/advice`;
+
+window.addEventListener("load", () => {
+  axios.get(apiUrl).then(randomAdvice);
+});
+
 btn.addEventListener("click", (event) => {
-  event.preventDefault();
   let apiUrl = `https://api.adviceslip.com/advice`;
   axios.get(apiUrl).then(showAdvice);
 });
