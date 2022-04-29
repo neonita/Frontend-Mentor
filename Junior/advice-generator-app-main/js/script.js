e715028409d8e4f5c57c5f0c75d5941155d95af0
Neonita <neonita888@gmail.com>
2022-04-28 23:33:34 -0400

    Import Axios

--- Junior/advice-generator-app-main/js/script.js
+++ Junior/advice-generator-app-main/js/script.js
@@ -1,5 +1,15 @@
 let adviceNum = document.querySelector(".card__number");
-let advice = document.querySelector(".card__advice-generated");
+let adviceText = document.querySelector(".card__advice-generated");
 let btn = document.querySelector(".card__btn");
 
-console.log(btn);
+const showAdvice = (response) => {
+  const slip = response.data.slip;
+  const adviceId = slip.id;
+  const adviceTxt = slip.advice;
+};
+
+btn.addEventListener("click", (event) => {
+  event.preventDefault();
+  let apiUrl = `https://api.adviceslip.com/advice`;
+  axios.get(apiUrl).then(showAdvice);
+});
