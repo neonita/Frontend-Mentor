55bd27beb3b6a8696e861690aef010a3c0cbc2ba
Neonita <neonita888@gmail.com>
2022-05-10 23:46:45 -0400

    Set project's custom colors and fonts

--- Junior/insure-land-page/src/App.js
+++ Junior/insure-land-page/src/App.js
@@ -1,6 +1,6 @@
 function App() {
   return (
-    <div className="">
+    <div className="font-body text-primaryViolet">
       <h1>Hi from app!</h1>
     </div>
   );
--- Junior/insure-land-page/tailwind.config.js
+++ Junior/insure-land-page/tailwind.config.js
@@ -1,6 +1,13 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    colors: {
+      primaryViolet: "hsl(256, 26%, 20%)",
+      primaryBlue: "hsl(216, 30%, 68%)",
+      neutralViolet: "hsl(270, 9%, 17%)",
+      neutralGrayViolet: "hsl(273, 4%, 51%)",
+      neutralGray: "hsl(0, 0%, 98%)",
+    },
     extend: {},
   },
   plugins: [],
