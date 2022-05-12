2348bd7efe29166f9596b977b5e78b26b1c20cc5
Neonita <neonita888@gmail.com>
2022-05-12 00:39:14 -0400

    Create Footer component, style and structure the footer's mobile resolution

--- Junior/insure-land-page/src/App.js
+++ Junior/insure-land-page/src/App.js
@@ -1,6 +1,7 @@
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Information from "./components/Information";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Header />
       <Hero />
       <Information />
+      <Footer />
     </div>
   );
 }
