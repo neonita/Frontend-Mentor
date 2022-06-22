397e23ccf05faa724bf7cc660343994de54f69f9
Neonita <neonita888@gmail.com>
2022-06-22 18:58:49 -0400

    Import script, styles and initialize AOS

--- Junior/easybank-landing-page-main/src/App.js
+++ Junior/easybank-landing-page-main/src/App.js
@@ -1,10 +1,17 @@
 import Navbar from "./components/Navbar/Navbar";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    AOS.init();
+  }, []);
+
   return (
-    <div>
+    <>
       <Navbar />
-    </div>
+    </>
   );
 }
 
