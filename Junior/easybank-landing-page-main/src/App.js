e71d9cab317e8ca15db11067e75b722a39bb5cb5
Neonita <neonita888@gmail.com>
2022-06-22 01:41:12 -0400

    Reset project folder, set CSS variables

--- Junior/easybank-landing-page-main/src/App.js
+++ Junior/easybank-landing-page-main/src/App.js
@@ -1,5 +1,11 @@
+import Navbar from "./components/Navbar/Navbar";
+
 function App() {
-  return <div>easybank</div>;
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
 }
 
 export default App;
