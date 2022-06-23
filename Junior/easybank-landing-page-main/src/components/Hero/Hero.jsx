601dd8c81639adcfa2ea87ef33b82d32feb88b47
Neonita <neonita888@gmail.com>
2022-06-22 23:07:00 -0400

    Add animation (AOS) to Hero's contents

--- Junior/easybank-landing-page-main/src/components/Hero/Hero.jsx
+++ Junior/easybank-landing-page-main/src/components/Hero/Hero.jsx
@@ -6,7 +6,11 @@ const Hero = () => {
   return (
     <section className={styles.container}>
       <div className={styles.content}>
-        <aside className={styles.information}>
+        <aside
+          className={styles.information}
+          data-aos="fade-up"
+          data-aos-duration="2000"
+        >
           <h2 className={styles.heading}>Next generation digital banking</h2>
           <p className={styles.text}>
             Take your financial life online. Your Easybank account will be a
@@ -22,6 +26,8 @@ const Hero = () => {
         src={Mockup}
         alt="Illustration of Easybank's app mockup."
         className={styles.img}
+        data-aos="fade-up"
+        data-aos-duration="2000"
       />
     </section>
   );
