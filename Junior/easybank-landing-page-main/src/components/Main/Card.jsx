737935edd96df7a9a4c71a14f80cf5fd8ae1eb18
Neonita <neonita888@gmail.com>
2022-06-27 23:38:46 -0400

    Create Card.jsx component to store individual features' info

---
+++ Junior/easybank-landing-page-main/src/components/Main/Card.jsx
@@ -0,0 +1,14 @@
+// import Img from "../../assets/images/icon-online.svg";
+import styles from "./Card.module.css";
+
+const Card = ({ data }) => {
+  return (
+    <div className={styles.card}>
+      <img src={data.icon} alt={data.title} />
+      <h3 className={styles.cardTitle}>{data.title}</h3>
+      <p className={styles.cardText}>{data.description}</p>
+    </div>
+  );
+};
+
+export default Card;
