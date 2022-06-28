9c86868d926c71c33fe9862db44ca74820974d3b
Neonita <neonita888@gmail.com>
2022-06-28 12:22:51 -0400

    Style Card component

--- Junior/easybank-landing-page-main/src/components/Main/Card.jsx
+++ Junior/easybank-landing-page-main/src/components/Main/Card.jsx
@@ -4,7 +4,7 @@ import styles from "./Card.module.css";
 const Card = ({ data }) => {
   return (
     <div className={styles.card}>
-      <img src={data.icon} alt={data.title} />
+      <img src={data.icon} alt={data.title} className={styles.cardImg} />
       <h3 className={styles.cardTitle}>{data.title}</h3>
       <p className={styles.cardText}>{data.description}</p>
     </div>
