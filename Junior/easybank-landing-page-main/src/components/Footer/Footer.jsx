46610186dd5a578629beee0c778f778780eaef13
Neonita <neonita888@gmail.com>
2022-06-29 18:50:29 -0400

    Create Footer component

--- Junior/easybank-landing-page-main/src/components/Footer/Footer.jsx
+++ Junior/easybank-landing-page-main/src/components/Footer/Footer.jsx
@@ -1,8 +1,10 @@
+import Logo2 from ".././Logo/Logo2";
 import styles from "./Footer.module.css";
 
 const Footer = () => {
   return (
     <footer className={styles.footer}>
+      <Logo2 className={styles.logo} />
       <p className={styles.p}>I am a footer! 🌼</p>
     </footer>
   );
