15825ec0b61487941ea340ca598446776a6d095a
Neonita <neonita888@gmail.com>
2022-06-28 21:57:38 -0400

    Style and structure layout of Articles and Articles' card components

--- Junior/easybank-landing-page-main/src/components/Articles/Card.jsx
+++ Junior/easybank-landing-page-main/src/components/Articles/Card.jsx
@@ -1,17 +1,20 @@
 import styles from "./Card.module.css";
+import Img from "../../assets/images/image-currency.jpg";
 
 const Card = () => {
   return (
     <div className={styles.card}>
-      <img src="/" alt="/" className={styles.img} />
-      <small className={styles.author}>By Claire Robinson</small>
-      <h4 className={styles.title}>
-        Receive money in any currency with no fees
-      </h4>
-      <p className={styles.textPreview}>
-        The world is getting smaller and we're becoming more mobile. So why
-        should you be force to only receive money in a single...
-      </p>
+      <img src={Img} alt="/" className={styles.img} />
+      <div className={styles.content}>
+        <small className={styles.author}>By Claire Robinson</small>
+        <h4 className={styles.title}>
+          Receive money in any currency with no fees
+        </h4>
+        <p className={styles.textPreview}>
+          The world is getting smaller and we're becoming more mobile. So why
+          should you be force to only receive money in a single...
+        </p>
+      </div>
     </div>
   );
 };
