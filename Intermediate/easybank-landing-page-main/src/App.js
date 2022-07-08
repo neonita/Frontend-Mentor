452b8b623f2c0b936ec041ea94f8644fb42b6a35
Neonita <neonita888@gmail.com>
2022-07-08 19:53:22 -0400

    Create css variables based on project's style guide

--- Intermediate/easybank-landing-page-main/src/App.js
+++ Intermediate/easybank-landing-page-main/src/App.js
@@ -1,12 +1,9 @@
 import Logo from "./components/Logo/Logo";
-import { IconHamburger } from "./assets/icons/";
 
 function App() {
   return (
     <div>
       <Logo />
-      <IconHamburger />
-      {/* <IconClose /> */}
     </div>
   );
 }
