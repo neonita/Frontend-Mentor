e193465a2a404f817fbb376bd47ecafa8817ad92
Neonita <neonita888@gmail.com>
2022-07-08 19:23:39 -0400

    Convert provided svg icons into components

--- Intermediate/easybank-landing-page-main/src/App.js
+++ Intermediate/easybank-landing-page-main/src/App.js
@@ -1,9 +1,12 @@
 import Logo from "./components/Logo/Logo";
+import { IconHamburger } from "./assets/icons/";
 
 function App() {
   return (
     <div>
       <Logo />
+      <IconHamburger />
+      {/* <IconClose /> */}
     </div>
   );
 }
