675b8146392f6346c7a8319996515742fa82e93b
Neonita <neonita888@gmail.com>
2022-07-08 20:31:15 -0400

    Create Navbar folder to store Navbar components

--- Intermediate/easybank-landing-page-main/src/App.js
+++ Intermediate/easybank-landing-page-main/src/App.js
@@ -1,9 +1,11 @@
 import Logo from "./components/Logo/Logo";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <div>
       <Logo />
+      <Navbar />
     </div>
   );
 }
