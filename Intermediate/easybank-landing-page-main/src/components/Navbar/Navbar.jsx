03d91ca24ceb82ff2cf1e86c027edc71117a33ca
Neonita <neonita888@gmail.com>
2022-07-11 15:31:21 -0400

    Structure and style large screen's Navbar component

--- Intermediate/easybank-landing-page-main/src/components/Navbar/Navbar.jsx
+++ Intermediate/easybank-landing-page-main/src/components/Navbar/Navbar.jsx
@@ -1,7 +1,54 @@
 import React from "react";
+import Button from "../Button/Button";
+import Logo from "../Logo/Logo";
+import styles from "./Navbar.module.css";
 
 const Navbar = () => {
-  return <header>Navbar</header>;
+  return (
+    <header className={styles.header}>
+      <div className={styles.header__container}>
+        <h1 className={styles.logo__container}>
+          <a href="/">
+            <Logo fill="#2d314d" />
+          </a>
+        </h1>
+
+        <nav className={styles.nav}>
+          <ul className={styles.nav__list}>
+            <li className={styles.nav__item}>
+              <a href="/" className={styles.nav__link}>
+                Home
+              </a>
+            </li>
+            <li className={styles.nav__item}>
+              <a href="/" className={styles.nav__link}>
+                About
+              </a>
+            </li>
+            <li className={styles.nav__item}>
+              <a href="/" className={styles.nav__link}>
+                Contact
+              </a>
+            </li>
+            <li className={styles.nav__item}>
+              <a href="/" className={styles.nav__link}>
+                Blog
+              </a>
+            </li>
+            <li className={styles.nav__item}>
+              <a href="/" className={styles.nav__link}>
+                Careers
+              </a>
+            </li>
+          </ul>
+        </nav>
+
+        <div className={styles.btn}>
+          <Button />
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Navbar;
