56e5f8fba71a0d3fce6e083daa678efd434133bd
Neonita <neonita888@gmail.com>
2022-07-11 22:03:00 -0400

    Structure toggle element in MobileToggle component, with useState hook

--- Intermediate/easybank-landing-page-main/src/components/Navbar/MobileToggle.jsx
+++ Intermediate/easybank-landing-page-main/src/components/Navbar/MobileToggle.jsx
@@ -1,7 +1,24 @@
-import React from "react";
+import { useState } from "react";
+import { IconHamburger, IconClose } from "../../assets/icons";
+import styles from "./MobileToggle.module.css";
 
 const MobileToggle = () => {
-  return <div>X</div>;
+  const [open, setOpen] = useState(false);
+  console.log(open);
+
+  return (
+    <>
+      <div className={styles.icon__container} onClick={() => setOpen(!open)}>
+        {!open ? (
+          <IconHamburger />
+        ) : (
+          <div className={styles.iconClose}>
+            <IconClose />
+          </div>
+        )}
+      </div>
+    </>
+  );
 };
 
 export default MobileToggle;
