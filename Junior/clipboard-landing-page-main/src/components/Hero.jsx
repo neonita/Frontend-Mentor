c7b4552ea0c349000bcce135fc6c6181e5c88d7b
Neonita <neonita888@gmail.com>
2022-07-22 21:39:50 -0400

    Style theme button and create flex helper class

--- Junior/clipboard-landing-page-main/src/components/Hero.jsx
+++ Junior/clipboard-landing-page-main/src/components/Hero.jsx
@@ -2,7 +2,6 @@ const Hero = () => {
   return (
     <section className="hero">
       <div className="hero__logo"></div>
-
       <div className="hero__text">
         <h1>A history of everything you copy</h1>
         <p>
@@ -10,12 +9,16 @@ const Hero = () => {
           Instantly access your clipboard on all your devices.
         </p>
       </div>
-      <a href="/" className="btn btn--primary">
-        Download for iOS
-      </a>
-      <a href="/" className="btn btn--secondary">
-        Download for Mac
-      </a>
+
+      <div className="flex flex-jc-center flex-ai-center flex-col">
+        <a href="/" className="btn btn__primary">
+          Download for iOS
+        </a>{" "}
+        <br />
+        <a href="/" className="btn btn__secondary">
+          Download for Mac
+        </a>
+      </div>
     </section>
   );
 };
