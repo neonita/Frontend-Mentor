de72d57a36274c3936bc96118e3ddf404ce570b8
Neonita <neonita888@gmail.com>
2022-07-23 23:14:48 -0400

    Style hero section's background image for mobile, tablet, and desktop resolution

--- Junior/clipboard-landing-page-main/src/components/Hero.jsx
+++ Junior/clipboard-landing-page-main/src/components/Hero.jsx
@@ -3,11 +3,12 @@ import Buttons from "./Buttons";
 
 const Hero = () => {
   return (
-    <section className="hero flex flex-jc-sb flex-col flex-ai-center section-p">
+    <section className="hero flex flex-jc-sb flex-col flex-ai-center">
+      {/*.section-p */}
+      <div className="hero__bg"></div>
       <div className="hero__logo">
         <img src={Logo} alt="Clipboard Logo" />
       </div>
-
       <div className="hero__text">
         <h1>A history of everything you copy</h1>
         <p>
@@ -15,18 +16,7 @@ const Hero = () => {
           Instantly access your clipboard on all your devices.
         </p>
       </div>
-
       <Buttons />
-
-      {/* <div className="hero__btns ">
-        <a href="/" className="btn btn__primary">
-          Download for iOS
-        </a>{" "}
-        <br />
-        <a href="/" className="btn btn__secondary">
-          Download for Mac
-        </a>
-      </div> */}
     </section>
   );
 };
