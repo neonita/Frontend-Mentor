606e549ee741af31780b11c8c63e1f3d9359a5db
Neonita <neonita888@gmail.com>
2022-07-24 00:11:36 -0400

    Store all list-type data in a data.js file, to loop through.

---
+++ Junior/clipboard-landing-page-main/src/data.js
@@ -0,0 +1,38 @@
+const data = [
+  [
+    {
+      title: "Quick Search",
+      description:
+        "Easily search your snippets by content, category, web address, application, and more.",
+    },
+    {
+      title: "iCloud Sync",
+      description:
+        "Instantly saves and syncs snippets across all your devices.",
+    },
+    {
+      title: "Complete History",
+      description:
+        "Retrieve any snippets from the first moment you started using the app.",
+    },
+  ],
+  [
+    {
+      title: "Create blacklists",
+      description:
+        "Ensure sensitive information never makes its way to your clipboard by excluding certain sources.",
+    },
+    {
+      title: "Plain text snippets",
+      description:
+        "Remove unwanted formatting from copied text for a consistent look.",
+    },
+    {
+      title: "Sneak preview",
+      description:
+        " Quick preview of all snippets on your Clipboard for easy access.",
+    },
+  ],
+];
+
+export default data;
