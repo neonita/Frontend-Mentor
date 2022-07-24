35846fa6fd76977d7f89c41f61c7667bad52dbad
Neonita <neonita888@gmail.com>
2022-07-24 00:11:52 -0400

    Structure html semantic of About section

--- Junior/clipboard-landing-page-main/src/components/About.jsx
+++ Junior/clipboard-landing-page-main/src/components/About.jsx
@@ -1,5 +1,35 @@
+import data from "../data.js";
+
 const About = () => {
-  return <div>About</div>;
+  console.log(data);
+  return (
+    <section className="about">
+      <div className="container">
+        <div className="about__text">
+          <h1>Keep track of your snippets</h1>
+          <p>
+            Clipboard instantly stores any item you copy in the cloud, meaning
+            you can access your snippets immediately on all your devices. Our
+            Mac and iOS apps will help you organize everything.
+          </p>
+        </div>
+
+        <div className="about__image"></div>
+
+        <div className="about__features">
+          <ul>
+            <li>
+              <h2>Quick Search</h2>
+              <p>
+                Easily search your snippets by content, category, web address,
+                application, and more.
+              </p>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default About;
