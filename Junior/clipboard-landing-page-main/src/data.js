9f43ca8ffa13072bba99b40e676e1f268607aff8
Neonita <neonita888@gmail.com>
2022-07-24 15:08:41 -0400

    Update the data.js file by giving each object an id to provide a key to its potential list item

--- Junior/clipboard-landing-page-main/src/data.js
+++ Junior/clipboard-landing-page-main/src/data.js
@@ -1,16 +1,19 @@
 const data = [
   [
     {
+      id: 0,
       title: "Quick Search",
       description:
         "Easily search your snippets by content, category, web address, application, and more.",
     },
     {
+      id: 1,
       title: "iCloud Sync",
       description:
         "Instantly saves and syncs snippets across all your devices.",
     },
     {
+      id: 2,
       title: "Complete History",
       description:
         "Retrieve any snippets from the first moment you started using the app.",
@@ -18,16 +21,19 @@ const data = [
   ],
   [
     {
+      id: 0,
       title: "Create blacklists",
       description:
         "Ensure sensitive information never makes its way to your clipboard by excluding certain sources.",
     },
     {
+      id: 1,
       title: "Plain text snippets",
       description:
         "Remove unwanted formatting from copied text for a consistent look.",
     },
     {
+      id: 2,
       title: "Sneak preview",
       description:
         " Quick preview of all snippets on your Clipboard for easy access.",
