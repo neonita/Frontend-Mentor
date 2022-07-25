d1190d9c692549e6eb2b478163059d6aaffd183e
Neonita <neonita888@gmail.com>
2022-07-24 23:34:53 -0400

    Set horizontal and vertical padding to about section for mobile resolution

--- Junior/clipboard-landing-page-main/src/components/About.jsx
+++ Junior/clipboard-landing-page-main/src/components/About.jsx
@@ -3,29 +3,27 @@ import data from "../data.js";
 const About = () => {
   console.log(data);
   return (
-    <section className="about">
-      <div className="container--px">
-        <div className="about__text">
-          <h1>Keep track of your snippets</h1>
-          <p>
-            Clipboard instantly stores any item you copy in the cloud, meaning
-            you can access your snippets immediately on all your devices. Our
-            Mac and iOS apps will help you organize everything.
-          </p>
-        </div>
+    <section className="about container--px container--py">
+      <div className="about__text">
+        <h1>Keep track of your snippets</h1>
+        <p>
+          Clipboard instantly stores any item you copy in the cloud, meaning you
+          can access your snippets immediately on all your devices. Our Mac and
+          iOS apps will help you organize everything.
+        </p>
+      </div>
 
-        <div className="about__image"></div>
+      <div className="about__image"></div>
 
-        <div className="about__features">
-          <ul>
-            {data[0].map((x) => (
-              <li key={x.id}>
-                <h2>{x.title}</h2>
-                <p>{x.description}</p>
-              </li>
-            ))}
-          </ul>
-        </div>
+      <div className="about__features">
+        <ul className="flex flex-col flex-jc-sb">
+          {data[0].map((x) => (
+            <li key={x.id}>
+              <h2>{x.title}</h2>
+              <p>{x.description}</p>
+            </li>
+          ))}
+        </ul>
       </div>
     </section>
   );
