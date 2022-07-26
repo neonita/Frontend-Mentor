0796960e9137e509c63eeb5035d25b21a76d7acf
Neonita <neonita888@gmail.com>
2022-07-26 00:12:34 -0400

    Create Preview component, structure html semantic and style elements on Preview section for mobile resolution

--- Junior/clipboard-landing-page-main/src/App.js
+++ Junior/clipboard-landing-page-main/src/App.js
@@ -1,11 +1,13 @@
 import About from "./components/About";
 import Hero from "./components/Hero";
+import Preview from "./components/Preview";
 
 function App() {
   return (
     <>
       <Hero />
       <About />
+      <Preview />
     </>
   );
 }
---
+++ Junior/clipboard-landing-page-main/src/components/Preview.jsx
@@ -0,0 +1,19 @@
+const Preview = () => {
+  return (
+    <section className="preview padding--x padding--y">
+      <div className="container">
+        <div className="preview__text">
+          <h2 className="h h-2">Access Clipboard anywhere</h2>
+          <p className="txt txt-2">
+            Whether you’re on the go, or at your computer, you can access all
+            your Clipboard snippets in a few simple clicks.
+          </p>
+        </div>
+
+        <div className="preview__image"></div>
+      </div>
+    </section>
+  );
+};
+
+export default Preview;
