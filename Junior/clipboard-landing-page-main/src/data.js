7b2acce46d6f6e4d573550f259bc01a800d99993
Neonita <neonita888@gmail.com>
2022-07-27 20:51:22 -0400

    Import png's and svg's in data.js to store in their respective array object

--- Junior/clipboard-landing-page-main/src/data.js
+++ Junior/clipboard-landing-page-main/src/data.js
@@ -1,3 +1,12 @@
+import IconBlacklist from "./assets/icons/icon-blacklist.svg";
+import IconPreview from "./assets/icons/icon-preview.svg";
+import IconText from "./assets/icons/icon-text.svg";
+import Google from "./assets/images/logo-google.png";
+import HP from "./assets/images/logo-hp.png";
+import IBM from "./assets/images/logo-ibm.png";
+import Microsoft from "./assets/images/logo-microsoft.png";
+import VG from "./assets/images/logo-vector-graphics.png";
+
 const data = [
   [
     {
@@ -22,22 +31,35 @@ const data = [
   [
     {
       id: 0,
+      svg: <IconBlacklist />,
+      alt: "Blacklist icon",
       title: "Create blacklists",
       description:
         "Ensure sensitive information never makes its way to your clipboard by excluding certain sources.",
     },
     {
       id: 1,
+      svg: <IconText />,
+      alt: "Text icon",
       title: "Plain text snippets",
       description:
         "Remove unwanted formatting from copied text for a consistent look.",
     },
     {
       id: 2,
+      svg: <IconPreview />,
+      alt: "Preview icon",
       title: "Sneak preview",
       description:
         " Quick preview of all snippets on your Clipboard for easy access.",
     },
+  ],
+  [
+    { id: 0, png: <Google />, alt: "Google logo" },
+    { id: 1, png: <HP />, alt: "HP logo" },
+    { id: 2, png: <IBM />, alt: "IBM logo" },
+    { id: 3, png: <Microsoft />, alt: "Microsoft logo" },
+    { id: 4, png: <VG />, alt: "Vector Graphics logo" },
   ],
 ];
 
