ba9ce70cc41b8813a4c10bc7d4122d1a6fbc3c4c
Neonita <neonita888@gmail.com>
2022-07-29 00:55:50 -0400

    Create Footer component, and _footer.scss

--- Junior/clipboard-landing-page-main/src/App.js
+++ Junior/clipboard-landing-page-main/src/App.js
@@ -3,6 +3,7 @@ import Hero from "./components/Hero";
 import Preview from "./components/Preview";
 import Services from "./components/Services";
 import CTA from "./components/CTA";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Preview />
       <Services />
       <CTA />
+      <Footer />
     </>
   );
 }
---
+++ Junior/clipboard-landing-page-main/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import Logo from "../assets/icons/logo.svg";
+
+const Footer = () => {
+  return (
+    <footer>
+      <img src={Logo} alt="Clipboard logo" />
+      <p>footer</p>
+    </footer>
+  );
+};
+
+export default Footer;
