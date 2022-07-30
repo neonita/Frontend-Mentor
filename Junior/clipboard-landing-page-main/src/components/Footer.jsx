c1b0f18742fada4e9543960821e930070873a9cc
Neonita <neonita888@gmail.com>
2022-07-30 01:07:01 -0400

    Style footer layout for mobile, tablet, and desktop resolution with css flex and grid

--- Junior/clipboard-landing-page-main/src/components/Footer.jsx
+++ Junior/clipboard-landing-page-main/src/components/Footer.jsx
@@ -5,69 +5,71 @@ import IconInstagram from "../assets/icons/icon-instagram.svg";
 
 const Footer = () => {
   return (
-    <footer className="footer padding--x padding--y flex flex-col flex-ai-center">
-      <img src={Logo} alt="Clipboard logo" className="footer__logo" />
+    <footer className="footer padding--x padding--y ">
+      <div className="container flex flex-col flex-ai-center">
+        <img src={Logo} alt="Clipboard logo" className="footer__logo" />
 
-      <div className="footer__links">
-        <ul className="flex flex-col flex-ai-center flex-jc-sb">
-          <li>
-            <a href="/" className="txt txt-4">
-              FAQs
-            </a>
-          </li>
-          <li>
-            <a href="/" className="txt txt-4">
-              Contact Us
-            </a>
-          </li>
-          <li>
-            <a href="/" className="txt txt-4">
-              Privacy Policy
-            </a>
-          </li>
-          <li>
-            <a href="/" className="txt txt-4">
-              Press Kit
-            </a>
-          </li>
-          <li>
-            <a href="/" className="txt txt-4">
-              Install Guide
-            </a>
-          </li>
-        </ul>
-      </div>
+        <div className="footer__links">
+          <ul className="flex flex-col flex-ai-center flex-jc-sb">
+            <li>
+              <a href="/" className="txt txt-4">
+                FAQs
+              </a>
+            </li>
+            <li>
+              <a href="/" className="txt txt-4">
+                Contact Us
+              </a>
+            </li>
+            <li>
+              <a href="/" className="txt txt-4">
+                Privacy Policy
+              </a>
+            </li>
+            <li>
+              <a href="/" className="txt txt-4">
+                Press Kit
+              </a>
+            </li>
+            <li>
+              <a href="/" className="txt txt-4">
+                Install Guide
+              </a>
+            </li>
+          </ul>
+        </div>
 
-      <div className="footer__socials">
-        <ul className="flex flex-jc-sb">
-          <li>
-            <a
-              href="https://www.facebook.com/"
-              target="_blank"
-              rel="noreferrer noopener"
-            >
-              <img src={IconFacebook} alt="Facebook icon" />
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://twitter.com/"
-              target="_blank"
-              rel="noreferrer noopener"
-            >
-              <img src={IconTwitter} alt="Twitter icon" />
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://www.instagram.com/"
-              target="_blank"
-              rel="noreferrer noopener"
-            >
-              <img src={IconInstagram} alt="Instagram icon" />
-            </a>
-          </li>
-        </ul>
+        <div className="footer__socials">
+          <ul className="flex flex-jc-sb">
+            <li>
+              <a
+                href="https://www.facebook.com/"
+                target="_blank"
+                rel="noreferrer noopener"
+              >
+                <img src={IconFacebook} alt="Facebook icon" />
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://twitter.com/"
+                target="_blank"
+                rel="noreferrer noopener"
+              >
+                <img src={IconTwitter} alt="Twitter icon" />
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.instagram.com/"
+                target="_blank"
+                rel="noreferrer noopener"
+              >
+                <img src={IconInstagram} alt="Instagram icon" />
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
     </footer>
   );
