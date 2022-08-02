a98cbb6397ab7369c50bda566a2175b0f2b729f6
Neonita <neonita888@gmail.com>
2022-08-02 19:02:33 -0400

    Style header for mobile screens

--- Intermediate/url-shortening-api-main/src/components/Header/Header.jsx
+++ Intermediate/url-shortening-api-main/src/components/Header/Header.jsx
@@ -1,5 +1,28 @@
+import { useState } from "react";
+import { ReactComponent as Logo } from "../../assets/images/logo.svg";
+
 const Header = () => {
-  return <header>Header</header>;
+  const [open, isOpen] = useState(!open);
+  console.log(open);
+
+  return (
+    <header className="header">
+      {/* Screen reader only */}
+      <h1>Shortly</h1>
+
+      {/* Logo */}
+      <div className="header__logo-container">
+        <Logo className="logo" />
+      </div>
+
+      {/* Hamburger menu */}
+      <div className="header__hamburger">
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
