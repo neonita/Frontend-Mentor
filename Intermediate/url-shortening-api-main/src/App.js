110b740eb899abddacc5c115032e0e4836699818
Neonita <neonita888@gmail.com>
2022-08-02 13:04:55 -0400

    Create Header folder component and _header.scss sass file

--- Intermediate/url-shortening-api-main/src/App.js
+++ Intermediate/url-shortening-api-main/src/App.js
@@ -1,8 +1,10 @@
+import Header from "./components/Header/Header";
+
 function App() {
   return (
-    <div>
-      <h1>url shortening app</h1>
-    </div>
+    <>
+      <Header />
+    </>
   );
 }
 
