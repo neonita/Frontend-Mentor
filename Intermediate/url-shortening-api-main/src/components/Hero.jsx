16c4c1d225836c925aab203926c5d2af7e49a68c
Neonita <neonita888@gmail.com>
2022-08-08 00:04:13 -0400

    Create Hero component and css sass file

---
+++ Intermediate/url-shortening-api-main/src/components/Hero.jsx
@@ -0,0 +1,13 @@
+const Hero = () => {
+  return (
+    <section className="hero">
+      <div className="hero__illustration"></div>
+
+      <div className="hero__text">
+        <h2>More than just shorter links</h2>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
