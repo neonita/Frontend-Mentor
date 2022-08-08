c650002934dbe4d98ec3c8383cfe7b9637e824d2
Neonita <neonita888@gmail.com>
2022-08-08 00:09:19 -0400

    Construct html structure of hero section

--- Intermediate/url-shortening-api-main/src/components/Hero.jsx
+++ Intermediate/url-shortening-api-main/src/components/Hero.jsx
@@ -3,8 +3,13 @@ const Hero = () => {
     <section className="hero">
       <div className="hero__illustration"></div>
 
-      <div className="hero__text">
+      <div className="description">
         <h2>More than just shorter links</h2>
+        <p className="txt txt-1">
+          Build your brand's recognition and get detailed insights on how your
+          links are performing.
+        </p>
+        <button>Get Started</button>
       </div>
     </section>
   );
